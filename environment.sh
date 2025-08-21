ff53c0dd737fd856c57883865f933211fd179d1b
Bill Milleker <winmill@yahoo.com>
2025-08-21 07:32:50 -0500

    for tokyo

--- environment.sh
+++ environment.sh
@@ -1,6 +1,19 @@
 #! /bin/bash
-#sudo ufw allow 2001/tcp
-#sudo ufw allow 2002/tcp
+# whoami
+# sudo ufw allow 2001/tcp
+# portainer
 sudo ufw allow 9443/tcp
+# dashboard
+#sudo ufw allow 8501/tcp
+# API
+#sudo ufw allow 8000/tcp
+# PostgresSQL
+#sudo ufw allow 5432/tcp
+# EMQX dashboard
+#sudo ufw allow 18083/tcp
 sudo ufw reload
 sudo ufw status verbose
+
+# manual steps
+# /etc/ssh/sshd_config
+#   PasswordAuthentication no
